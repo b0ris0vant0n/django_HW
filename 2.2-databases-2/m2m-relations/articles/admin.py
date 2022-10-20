b6c5400bc75c3042c69485e6d83e741e71c57ba7
Anton Borisov <borisov2011@gmail.com>
2022-10-20 22:01:03 +0800

    HomeWork

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super(RelationshipInlineFormset, self).clean()
+        total_checked = 0
+        for form in self.forms:
+            if not form.is_valid():
+                return
+            if form.cleaned_data and not form.cleaned_data.get('DELETE'):
+                if form.cleaned_data['is_main']:
+                    total_checked += 1
+        if total_checked > 1:
+            raise ValidationError("У же есть один основной раздел, можно выбрать один!")
+
+        if total_checked < 1:
+            raise ValidationError("Вам необходимо выбрать один основной раздел!")
+
+        return super().clean()
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +23,8 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Scope(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
