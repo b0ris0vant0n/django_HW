0936c9fa93fe2783502c1f577d515c3d61ea49b2
Anton Borisov <borisov2011@gmail.com>
2022-10-25 19:12:29 +0800

    Homework for CRUD

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,27 @@
 from rest_framework import serializers
 
+from logistic.models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address' , 'products', 'positions']
+
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,6 +33,9 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(product=position['product'], stock=stock,
+                                        quantity=position['quantity'], price=position['price'])
 
         return stock
 
@@ -39,5 +49,7 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            defaults = {'quantity': position['quantity'], 'price': position['price']}
+            StockProduct.objects.update_or_create(product=position['product'], stock=stock, defaults=defaults)
         return stock
